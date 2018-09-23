6a13bfdbbb22ff313f77a06d55f4ed45f543d311
Balázs Richer <richer.balazs@gmail.com>
2018-09-23 10:06:26 +0200

    Fill fixture with players.

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,7 +2,8 @@
 
 namespace App\DataFixtures;
 
-use App\Entity\Score;
+use App\Entity\Player;
+use Faker;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -10,36 +11,13 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $score1 = new Score();
-        $score1->setField('d20');
-        $score1->setValue(40);
+        $faker = Faker\Factory::create();
 
-        $score2 = new Score();
-        $score2->setField('t20');
-        $score2->setValue(60);
-
-        $score3 = new Score();
-        $score3->setField('db');
-        $score3->setValue(50);
-
-        $score4 = new Score();
-        $score4->setField('b');
-        $score4->setValue(25);
-
-        $score5 = new Score();
-        $score5->setField('20');
-        $score5->setValue(20);
-
-        $score6 = new Score();
-        $score6->setField('13');
-        $score6->setValue(14);
-
-        $manager->persist($score1);
-        $manager->persist($score2);
-        $manager->persist($score3);
-        $manager->persist($score4);
-        $manager->persist($score5);
-        $manager->persist($score6);
+        for ($i = 1; $i < 12; $i++) {
+            $player = new Player();
+            $player->setName($faker->name);
+            $manager->persist($player);
+        }
 
         $manager->flush();
     }
