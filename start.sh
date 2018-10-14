c91d91650af869e0a2dea2cdb9a329108e4b74b1
Balázs Richer <richer.balazs@gmail.com>
2018-10-14 11:50:51 +0200

    Move API to 8080 from host 80 in the conatiner, for easier CORS development.

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-docker run -d -ti -p 80:8080 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp darts-scoreboard-backend-api php -S 0.0.0.0:8080 -t public
+docker run -d -ti -p 8080:80 --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp darts-scoreboard-backend-api php -S 0.0.0.0 -t public
